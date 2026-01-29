c0d1ed165a7e269dfa441397dcf0483f19d89b3b
Brett Sartain <brett.sartain@comporium.com>
2026-01-29 13:41:53 -0500

    Adjusted styles. Created Meta Data for links

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,21 +2,38 @@ import type { Metadata } from "next";
 import "./globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import { Arsenal, Source_Sans_3 } from "next/font/google";
-
-const arsenal = Arsenal({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-  style: ["normal", "italic"],
-});
+import { Source_Sans_3 } from "next/font/google";
 
 const sourceSans = Source_Sans_3({
   subsets: ["latin"],
 });
 
+// export const metadata: Metadata = {
+//   title: "Grace and Grit - Rock Hill Spin Studio",
+//   description: "Spin cycling classes at Grace and Grit",
+// };
+
 export const metadata: Metadata = {
-  title: "Grace and Grit - Rock Hill Spin Studio",
-  description: "Spin cycling classes at Grace and Grit",
+  metadataBase: new URL("https://rockhillspinstudio.com"),
+
+  title: "Grace & Grit Spin Studio",
+  description: "High-energy spin cycling classes at Grace & Grit in Rock Hill.",
+
+  openGraph: {
+    type: "website",
+    url: "https://rockhillspinstudio.com",
+    title: "Grace & Grit Spin Studio",
+    description: "High-energy spin cycling classes at Grace & Grit in Rock Hill.",
+    siteName: "Grace & Grit",
+    images: [
+      {
+        url: "/og-facebook.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Grace & Grit Spin Studio",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
